bfaaaf19bd2a844a6fe91b7cb3e1f03d9c8587dd
czerwonamaupa <czerwonamaupa@protonmail.com>
2018-12-20 19:20:10 +0100

    Improve hyphenation.

--- filters/hyphenate.py
+++ filters/hyphenate.py
@@ -1,12 +1,16 @@
+import re
 from pandocfilters import Str, toJSONFilter
 from pyphen import Pyphen
 
-dic = Pyphen(lang='pl_PL', left=4, right=4)
+dic = Pyphen(lang='pl_PL', left=3, right=3)
+
+word_detection_pattern = re.compile(r'\w{7,}', re.UNICODE)
 
 def hyphenate(key, value, format, meta):
     if key == 'Str':
-        hyphenated = dic.inserted(value, hyphen='­')
-        return Str(hyphenated)
+        return Str(word_detection_pattern.sub(
+            lambda match: dic.inserted(match.group(0), hyphen='­'),
+            value))
 
 if __name__ == "__main__":
     toJSONFilter(hyphenate)
