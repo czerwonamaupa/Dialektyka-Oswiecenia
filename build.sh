a86ae9c600aac2a393ea6cd4908b15fa9becaf5c
czerwonamaupa <czerwonamaupa@protonmail.com>
2018-12-20 19:56:40 +0100

    Switch build.sh to default makefile target.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ls styles/* chapters/* template.t filters/* cover.jpg | entr make index.html ebooks/dialektyka-oswiecenia.mobi
+ls styles/* chapters/* template.t filters/* cover.jpg | entr make
